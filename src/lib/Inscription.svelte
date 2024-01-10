89f3ff681306846dbe3322f6b1eed4a56caebf25
francoislg <francois.lguillemette@gmail.com>
2024-01-10 14:48:16 -0500

    temp fix date limite

--- src/lib/Inscription.svelte
+++ src/lib/Inscription.svelte
@@ -40,31 +40,29 @@
 	}
 </script>
 
-{#if !dateLimite || dateLimite > plusTard}
-	<div class="flex flex-col gap-2 bg-green-100 rounded-lg p-4 w-full">
-		<div class="flex justify-between">
-			<h4 class={sHeaders.h4()}>{@render title()}</h4>
-			{#if dateLimite}
-				<div class="hidden sm:block">Limite: {formatDate(dateLimite)}</div>
-			{/if}
-		</div>
-		<button class="inline sm:hidden {sButton()}" on:click={toggleHidden}>
-			{hidden ? 'Voir' : 'Cacher'} les détails</button
-		>
-		<div class:hidden class="sm:block flex flex-col">
-			{#if dateLimite}
-				<div class="inline sm:hidden">Limite: {formatDate(dateLimite)}</div>
-			{/if}
-			<div>
-				{@render texte()}
-			</div>
-		</div>
-		<div class="text-center">
-			{#if !dateLimite || dateLimite > maintenant}
-				<Link href={lien}>Inscription ici</Link>
-			{:else}
-				<div class="text-center">Inscription terminée</div>
-			{/if}
+<div class="flex flex-col gap-2 bg-green-100 rounded-lg p-4 w-full">
+	<div class="flex justify-between">
+		<h4 class={sHeaders.h4()}>{@render title()}</h4>
+		{#if dateLimite}
+			<div class="hidden sm:block">Limite: {formatDate(dateLimite)}</div>
+		{/if}
+	</div>
+	<button class="inline sm:hidden {sButton()}" on:click={toggleHidden}>
+		{hidden ? 'Voir' : 'Cacher'} les détails</button
+	>
+	<div class:hidden class="sm:block flex flex-col">
+		{#if dateLimite}
+			<div class="inline sm:hidden">Limite: {formatDate(dateLimite)}</div>
+		{/if}
+		<div>
+			{@render texte()}
 		</div>
 	</div>
-{/if}
+	<div class="text-center">
+		{#if !dateLimite || dateLimite > maintenant}
+			<Link href={lien}>Inscription ici</Link>
+		{:else}
+			<div class="text-center">Inscription terminée</div>
+		{/if}
+	</div>
+</div>
