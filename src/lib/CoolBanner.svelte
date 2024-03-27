e85cd773592fb627d9a1c992498bec5edbd6d7a3
francoislg <francois.lguillemette@gmail.com>
2024-03-27 14:11:17 -0400

    Tournoi de Smash

---
+++ src/lib/CoolBanner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { text } = $props<{
+		text?: Snippet;
+	}>();
+</script>
+
+{#if text}
+	<div class="bg-green-100 border-green-300 border rounded-lg px-8 py-4 font-bold">
+		🏆 {@render text()} 🏆
+	</div>
+{/if}
